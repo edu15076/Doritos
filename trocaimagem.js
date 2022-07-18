5c41a6393b01a7fbf685000a6880f861a1aec2c1
Arthur-Teles <104380993+Arthur-Teles@users.noreply.github.com>
2022-07-18 18:45:03 -0300

    Create trocaimagem.js

---
+++ trocaimagem.js
@@ -0,0 +1,17 @@
+var trocaImagem1 = document.getElementById('clique1');
+
+trocaImagem1.addEventListener('click', function(trocaImagem1) {
+    trocaImagem1.target.classList.toggle('will-js');
+})
+
+var trocaImagem2 = document.getElementById('clique2');
+
+trocaImagem2.addEventListener('click', function(trocaImagem2) {
+    trocaImagem2.target.classList.toggle('greg-js');
+})
+
+var trocaImagem3 = document.getElementById('clique3');
+
+trocaImagem3.addEventListener('click', function(trocaImagem3) {
+    trocaImagem3.target.classList.toggle('mark-js');
+})
